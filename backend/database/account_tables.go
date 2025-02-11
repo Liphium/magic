f4bac5b42c45dbbedd8e97a7b2c9263e3005529b
Julian <70802809+Unbreathable@users.noreply.github.com>
2025-02-11 12:28:40 +0100

    Start on DB tables

--- backend/database/account_tables.go
+++ backend/database/account_tables.go
@@ -1,1 +1,17 @@
 package database
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Account struct {
+	ID       uuid.UUID
+	Username string
+	Email    string
+	Rank     uint
+
+	Created time.Time
+	Updated time.Time
+}
