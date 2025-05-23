e4a23364403ba34999fe254b43bf781c7708eefc
jan <212736953+tiemingo@users.noreply.github.com>
2025-05-24 00:18:14 +0200

    Barebone wroking tui

--- tui/main.go
+++ tui/main.go
@@ -2,139 +2,144 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"os"
-	"strings"
+	"log"
+	"sync"
 	"time"
 
-	"github.com/charmbracelet/bubbles/progress"
-	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+)
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type (
+	errMsg error
 )
 
 type model struct {
-	packages []string
-	index    int
+	textInput textinput.Model
+	err       error
 	width    int
 	height   int
-	spinner  spinner.Model
-	progress progress.Model
-	done     bool
 }
 
-var (
-	currentPkgNameStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("211"))
-	doneStyle           = lipgloss.NewStyle().Margin(1, 2)
-	checkMark           = lipgloss.NewStyle().Foreground(lipgloss.Color("42")).SetString("✓")
-)
+type sPipe struct {
+	mutex *sync.Mutex
+	items []string
+}
 
-func newModel() model {
-	p := progress.New(
-		progress.WithDefaultGradient(),
-		progress.WithWidth(40),
-		progress.WithoutPercentage(),
-	)
-	s := spinner.New()
-	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("63"))
+var pn = &sPipe{
+	mutex: &sync.Mutex{},
+	items: []string{},
+}
+
+var ctr = &sPipe{
+	mutex: &sync.Mutex{},
+	items: []string{},
+}
+
+func addItem(pn *sPipe, item string) {
+	pn.mutex.Lock()
+	defer pn.mutex.Unlock()
+
+	pn.items = append(pn.items, item)
+}
+
+func getItem(pn *sPipe) (string, bool){
+	pn.mutex.Lock()
+	defer pn.mutex.Unlock()
+	if len(pn.items) == 0 {
+		return "", false
+	}
+	item := pn.items[0]
+	pn.items = pn.items[1:]
+	return item, true
+}
+
+func runTui(pn *sPipe){
+	i := 0
+	for  {
+		time.Sleep(time.Millisecond * 100)
+		addItem(pn, "number of prints: " + fmt.Sprint(i))
+		if cmd, newCmd := getItem(ctr); newCmd{
+			switch cmd {
+			case "cat":
+				addItem(pn, "meow :3")
+			}
+			
+		}
+		i++
+	}
+}
+
+func initialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "Enter command"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 0
+
 	return model{
-		packages: getPackages(),
-		spinner:  s,
-		progress: p,
+		textInput: ti,
+		err:       nil,
 	}
 }
 
 func (m model) Init() tea.Cmd {
-	return tea.Batch(downloadAndInstall(m.packages[m.index]), m.spinner.Tick)
+	go runTui(pn)
+
+	return tea.Batch(textinput.Blink)
+}
+// remove if creates lag or not wanted
+func setUpdateTime() tea.Cmd {
+	d := time.Millisecond * time.Duration(100) // set update time in ms
+	return tea.Tick(d, func(t time.Time) tea.Msg {
+		return ""
+	})
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		m.width, m.height = msg.Width, msg.Height
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "esc", "q":
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
-		}
-	case installedPkgMsg:
-		pkg := m.packages[m.index]
-		if m.index >= len(m.packages)-1 {
-			// Everything's been installed. We're done!
-			m.done = true
-			return m, tea.Sequence(
-				tea.Printf("%s %s", checkMark, pkg), // print the last success message
-				tea.Quit,                            // exit the program
-			)
+		case tea.KeyEnter:
+			addItem(ctr, m.textInput.Value())
+			m.textInput.SetValue("")
 		}
 
-		// Update progress bar
-		m.index++
-		progressCmd := m.progress.SetPercent(float64(m.index) / float64(len(m.packages)))
-
-		return m, tea.Batch(
-			progressCmd,
-			tea.Printf("%s %s", checkMark, pkg),     // print success message above our program
-			downloadAndInstall(m.packages[m.index]), // download the next package
-		)
-	case spinner.TickMsg:
-		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
-		return m, cmd
-	case progress.FrameMsg:
-		newModel, cmd := m.progress.Update(msg)
-		if newModel, ok := newModel.(progress.Model); ok {
-			m.progress = newModel
-		}
-		return m, cmd
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
 	}
-	return m, nil
+
+	m.textInput.Width = m.width
+	m.textInput, cmd = m.textInput.Update(msg)
+
+	if itemToPrint, shouldPrint := getItem(pn); shouldPrint{
+		return m, tea.Batch(
+			tea.Println(itemToPrint),
+			cmd,
+			setUpdateTime(),
+		)
+	} else{
+		return m, tea.Batch(cmd, setUpdateTime(),)
+	}
 }
 
 func (m model) View() string {
-	n := len(m.packages)
-	w := lipgloss.Width(fmt.Sprintf("%d", n))
-
-	if m.done {
-		return doneStyle.Render(fmt.Sprintf("Done! Installed %d packages.\n", n))
-	}
-
-	pkgCount := fmt.Sprintf(" %*d/%*d", w, m.index, w, n)
-
-	spin := m.spinner.View() + " "
-	prog := m.progress.View()
-	cellsAvail := max(0, m.width-lipgloss.Width(spin+prog+pkgCount))
-
-	pkgName := currentPkgNameStyle.Render(m.packages[m.index])
-	info := lipgloss.NewStyle().MaxWidth(cellsAvail).Render("Installing " + pkgName)
-
-	cellsRemaining := max(0, m.width-lipgloss.Width(spin+info+prog+pkgCount))
-	gap := strings.Repeat(" ", cellsRemaining)
-
-	return spin + info + gap + prog + pkgCount
-}
-
-type installedPkgMsg string
-
-func downloadAndInstall(pkg string) tea.Cmd {
-	// This is where you'd do i/o stuff to download and install packages. In
-	// our case we're just pausing for a moment to simulate the process.
-	d := time.Millisecond * time.Duration(rand.Intn(500)) //nolint:gosec
-	return tea.Tick(d, func(t time.Time) tea.Msg {
-		return installedPkgMsg(pkg)
-	})
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func main() {
-	if _, err := tea.NewProgram(newModel()).Run(); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
-	}
+	return fmt.Sprint(
+		m.textInput.View(),
+	)
 }
