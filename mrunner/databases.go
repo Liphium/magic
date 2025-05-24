a7514d01f6dd898dec57fb003f633cb7949cbf93
Julian <70802809+Unbreathable@users.noreply.github.com>
2025-05-24 13:26:06 +0200

    Database defaults

--- mrunner/databases.go
+++ mrunner/databases.go
@@ -14,6 +14,31 @@ type Database struct {
 	port        uint   `json:"po"`
 }
 
+// Get the default password for the database type
+func (db *Database) DefaultPassword() string {
+	switch db.dbType {
+	case mconfig.DatabasePostgres:
+		return "postgres"
+	default:
+		return "admin"
+	}
+}
+
+// Get the default username for the database type
+func (db *Database) DefaultUsername() string {
+	switch db.dbType {
+	case mconfig.DatabasePostgres:
+		return "postgres"
+	default:
+		return "admin"
+	}
+}
+
+// Get the default name for the database using the runner
+func (db *Database) DefaultDatabaseName(runner *Runner) string {
+	return "mgc-" + runner.config + "-" + runner.profile + "-" + db.name
+}
+
 // Create a new database from one in the config
 func (r *Runner) CreateDatabaseFrom(db *mconfig.Database) {
 	r.databases = append(r.databases, &Database{
