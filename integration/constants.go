f0b4b7746fe8b8c0bd9ec994c9c74fc3c1b67ce9
Julian <unfehlerhaft@gmail.com>
2025-06-28 18:45:15 +0200

    Update to rc11

--- integration/constants.go
+++ integration/constants.go
@@ -1,4 +1,4 @@
 package integration
 
 // The current version of Magic to make sure the CLI and generated modules are always compatible
-const MagicVersion = "v1.0.0-rc10"
+const MagicVersion = "v1.0.0-rc11"
